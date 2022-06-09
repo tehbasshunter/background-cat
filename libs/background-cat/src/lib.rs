ca581d7140efeba3483943c684327189cd6a29b0
Petr Mrázek <peterix@gmail.com>
2022-06-09 18:30:22 +0200

    Recognize warning MultiMC adds to the log about Java 17

--- libs/background-cat/src/lib.rs
+++ libs/background-cat/src/lib.rs
@@ -129,13 +129,15 @@ fn forge_too_new_java(log: &str) -> Option<(&str, String)> {
 }
 
 fn one_seventeen_plus_java_too_old(log: &str) -> Option<(&str, String)> {
-    const UNSUPPORTED_CLASS_VERSION_ERROR: &str =
-        "java.lang.UnsupportedClassVersionError: net/minecraft/client/main/Main";
+    const UNSUPPORTED_CLASS_VERSION_ERROR: &str = "java.lang.UnsupportedClassVersionError: net/minecraft/client/main/Main";
     const FABRIC_JAVA_VERSION_ERROR: &str = "fabric requires {java @ [>=16]}";
     const FABRIC_JAVA_VERSION_ERROR_SEVENTEEN: &str = "fabric requires {java @ [>=17]}";
+    const JAVA_17_WARNING: &str = "Minecraft 1.18 Pre Release 2 and above require the use of Java 17";
+
     if log.contains(UNSUPPORTED_CLASS_VERSION_ERROR)
         || log.contains(FABRIC_JAVA_VERSION_ERROR)
         || log.contains(FABRIC_JAVA_VERSION_ERROR_SEVENTEEN)
+        || log.contains(JAVA_17_WARNING)
     {
         Some(("‼", RESPONSES.get("use-java-17")?.to_string()))
     } else {
